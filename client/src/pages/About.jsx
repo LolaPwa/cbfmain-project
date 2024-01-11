448698a3e0dcdefb74b9140f19d31fef68b8cf6d
Lola Pwasanga <lola.pwasanga@gmail.com>
2024-01-11 15:45:31 +0000

    correct wording

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -4,10 +4,10 @@ export default function About() {
   return (
     <div className='py-20 px-4 max-w-6xl mx-auto'>
       <h1 className='text-3xl font-bold mb-4 text-slate-800'> ABOUT US</h1>
-      <p className='mb-4 text-slate-700'> Lola Estates a leading christian housing association
+      <p className='mb-4 text-slate-700'> Lola Estates are a leading christian housing association
        specialising in reliably helping individuals and philantropist secure their owned
        empty properties. We do this by providing the opportunity for low income workers and families 
-        to occupy these properties at affordable costs albeit on a short term basis.</p>
+        to occupy these empty properties at affordable costs albeit on a short term basis.</p>
       <p className='mb-4 text-slate-700'>
         Our mission is to help our long standing property companies and individual clients achieve
         a lasting solution to the problems created by empty properties.
